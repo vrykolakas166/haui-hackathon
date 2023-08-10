656cbb6cb6800c548961bc86bc6826d38cb30233
vrykolakas166 <hhtb.bdh@gmail.com>
2023-08-10 13:29:29 +0700

    :<

--- codalab/apps/web/static/js/script.js
+++ codalab/apps/web/static/js/script.js
@@ -240,7 +240,7 @@ setInterval(focusNextElement, delay);
 
 // scroll event
 $(window).on("scroll", function() {
-  if(window.scrollY > 234){
+  if(window.scrollY < 234){
     $("#toTop").addClass("hidden");
   }
   else{
@@ -254,5 +254,5 @@ $("#toTop").on("click", function () {
   //html works for FFX but not Chrome
   //body works for Chrome but not FFX
   //This strange selector seems to work universally
-  $("html, body").animate({scrollTop: 0}, 1000);
+  $("html, body").animate({scrollTop: 0}, 350);
 });
