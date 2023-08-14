69b7a1e7bc7a71946a7878524299c0689bc330b2
vrykolakas166 <hhtb.bdh@gmail.com>
2023-08-14 15:30:39 +0700

    fix register button

--- codalab/apps/web/static/js/Competition.js
+++ codalab/apps/web/static/js/Competition.js
@@ -630,6 +630,11 @@ var Competition;
                 if ($('#participateButton').hasClass('disabledStatus')) {
                     return false;
                 }
+                // wait submit
+                $('#participateButton').addClass('disabledStatus');
+                $('#participateButton').html(`
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="white" d="M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z" opacity=".5"/><path fill="white" d="M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z"><animateTransform attributeName="transform" dur="1s" from="0 12 12" repeatCount="indefinite" to="360 12 12" type="rotate"/></path></svg>
+                    `);
                 $('#result').html('');
                 var values = $(this).serialize();
                 var competitionId = $('#competitionId').val();
@@ -648,6 +653,8 @@ var Competition;
                     },
                     error: function(jsXHR, textStatus, errorThrown) {
                         alert('There was a problem registering for this competition.');
+                        $('#participateButton').removeClass('disabledStatus');
+                        $('#participateButton').html("Register");
                     }
                 });
                 return false;
